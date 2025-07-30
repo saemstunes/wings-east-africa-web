08b4d16602330818bb2e97859a8e60537390c5ea
Saem's Tunes <saemstunes@gmail.com>
2025-07-30 19:57:47 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import { ClerkProvider } from '@clerk/clerk-react';
 import './index.css';
 
-const PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const PUBLISHABLE_KEY = "pk_test_b25lLW1hY2tlcmVsLTM3LmNsZXJrLmFjY291bnRzLmRldiQ";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
